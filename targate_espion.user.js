793654a105d1e7fc4dde07a2a53e68a114b28e10
Thilitium <thilitium@gmail.com>
2015-07-15 11:45:30 +0200

    azer

--- targate_espion.user.js
+++ targate_espion.user.js
@@ -4,7 +4,7 @@
 // @include     http://targate.fr/index.php?choix=centre_espionnage*
 // @include     http://www.targate.fr/index.php?choix=centre_espionnage*
 // @include     https://targate.fr/index.php?choix=centre_espionnage*
-// @version     1.0.1.28
+// @version     1.0.1.29
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @grant       GM_log
 // ==/UserScript==
@@ -86,7 +86,7 @@ var sortPlayers = function(table, players) {
 	for(var i=0;i<tTr.length/2;++i){
 		var attr = tTr[i*2].getAttribute('data-playerpoints');
 		var points = 0;
-		if(attr !== null) points = parseInt(attr);
+		if(attr !== null) points = parseFloat(attr);
 		var playerTr = {
 			trs 	: [$(tTr[i * 2]), $(tTr[i * 2 + 1])],
 			pts 	: points
@@ -107,10 +107,10 @@ var sortPlayers = function(table, players) {
 			}
 		}
 	}
-	GM_log(tabPts);
+
 	// Réorganisation du tableau des joueurs dans l'ordre.
-	/*for(var i=0;i<tabPts.length;++i) 
-		$tBody.prepend(tabPts[i].trs);*/
+	for(var i=0;i<tabPts.length;++i) 
+		$tBody.prepend(tabPts[i].trs);
 
 
 };
