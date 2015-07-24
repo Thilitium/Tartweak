ca36ed871289f61b6415df863e44a4904ab1a9b7
Thilitium <thilitium@gmail.com>
2015-07-24 10:13:23 +0200

    debug

--- targate_espion.user.js
+++ targate_espion.user.js
@@ -4,7 +4,7 @@
 // @include     http://targate.fr/index.php?choix=centre_espionnage*
 // @include     http://www.targate.fr/index.php?choix=centre_espionnage*
 // @include     https://targate.fr/index.php?choix=centre_espionnage*
-// @version     1.2.1.3
+// @version     1.2.1.4
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @grant       GM_log
@@ -270,7 +270,7 @@ var Notes = {
 	},
 	GetNote : function(playerName) {
 		var value = GM_SuperValue.get("note:" + playerName);
-		return (value!=='undefined') ? value:'';
+		return (value!==undefined) ? value : '';
 	},
 	AddEventsHandler : function() {
 		var self = this;
