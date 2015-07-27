a377e6e6394a87286a8dc379e43a0426e0a4c165
Thilitium <thilitium@gmail.com>
2015-07-27 14:10:18 +0200

    syntax

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.0.1
+// @version     0.0.0.2
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -41,7 +41,7 @@ var Data = {
 		});
 		if(callback !== undefined) callback(players);
 	}
-}
+};
 
 var Metier = {
 	StoreScores : function(players, callback) {
@@ -59,7 +59,7 @@ var Metier = {
 
 		if(callback!==undefined) callback(players);
 	}
-}
+};
 
 var UI = {
 	CreerChart : function(container, players) {
@@ -73,19 +73,18 @@ var UI = {
 				name 		: players[i].name,
 				type		: "spline",
 				dataPoints	: tScore
-			})
-			data.push()
-		};
+			});
+		}
 
 		var options = {
 			title				: {text: "Points des joueurs en fonction du temps : "},
 			animationEnabled	: true,
 			data 				: data
-		}
+		};
 
 		$(container).CanvasJSChart(options);
 	}
-}
+};
 
 // Récupération des joueurs et de leurs points.
 var players = [];
