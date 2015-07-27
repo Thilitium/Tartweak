f437df405e0d766c510e61089d77df010f7c5eff
Thilitium <thilitium@gmail.com>
2015-07-27 14:11:35 +0200

    bug

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.0.2
+// @version     0.0.0.3
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -88,14 +88,13 @@ var UI = {
 
 // Récupération des joueurs et de leurs points.
 var players = [];
-var self = this;
 var divContain = $("<div/>");
 $("body").prepend(divContain);
-self.Data.GetAllPlayers(
+Data.GetAllPlayers(
 	function(players) { 
-		self.Metier.StoreScores(players, 
+		Metier.StoreScores(players, 
 			function(players) {
-				self.CreerChart(self.divContain, players);
+				CreerChart(divContain, players);
 			}
 		);
 	}
