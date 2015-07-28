db2ddde1609c8cb0405687faa0b5710a00732566
Thilitium <thilitium@gmail.com>
2015-07-28 13:20:04 +0200

    v

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.0.3
+// @version     0.0.0.4
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -94,7 +94,7 @@ Data.GetAllPlayers(
 	function(players) { 
 		Metier.StoreScores(players, 
 			function(players) {
-				CreerChart(divContain, players);
+				UI.CreerChart(divContain, players);
 			}
 		);
 	}
