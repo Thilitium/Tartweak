fefdb5291549951ebaee0834d855428818deccdc
Thilitium <thilitium@gmail.com>
2015-08-03 11:29:17 +0200

    ttt

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.2.17
+// @version     0.0.2.18
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -123,7 +123,7 @@ var $body = $("body");
 // Récupération des joueurs et de leurs points.
 var players = [];
 var divMaster = $("<div class='tttdivgraph' style='position:absolute;height:300px;width:500px;z-index:999;'/>")
-var divContain = $("<div/>");
+var divContain = $("<div style='height:100%;width:100%;' />");
 
 divMaster.append(divContain);
 $body.prepend(divMaster);
@@ -132,7 +132,10 @@ divMaster.draggable();
 divMaster.resizable({
 	delay: 150,
 	stop: function(event, ui) {
-		$(this).CanvasJSChart().render();
+		chart = divContain.CanvasJSChart();
+		chart.options.width = divContain.width();
+		chart.options.height divContain.height();
+		chart.render();
 	}
 });
 
