c3b6497808f778ed282cba879a504bce4a02c2eb
Thilitium <thilitium@gmail.com>
2015-08-03 10:17:08 +0200

    t

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.2.4
+// @version     0.0.2.5
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -130,8 +130,8 @@ var style = $(
 	"</style>"
 );
 var divContain = $("<div class='tttdivgraph'/>");
-/*divContain.draggable();
-divContain.resizable();*/
+divContain.draggable();
+divContain.resizable();
 //$body.prepend(style);
 $body.prepend(divContain);
 Data.GetAllPlayers(
