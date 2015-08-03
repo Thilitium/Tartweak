cd93c1abcebf1587b0f4d5743a2bb1821033df35
Thilitium <thilitium@gmail.com>
2015-08-03 09:55:53 +0200

    v

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.2.1
+// @version     0.0.2.2
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -106,7 +106,7 @@ var UI = {
 				self._canUpdate = false;
 				$(this).CanvasJSChart(self._chartOptions); 
 				setTimeout(function() {
-					self._canUpdate = true,
+					self._canUpdate = true;
 				}, 1000);
 			}
 		});
