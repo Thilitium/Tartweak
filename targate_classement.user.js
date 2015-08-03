005bddae40379b56f3832467b8e9641362bc1011
Thilitium <thilitium@gmail.com>
2015-08-03 10:16:04 +0200

    test

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.2.3
+// @version     0.0.2.4
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -130,15 +130,15 @@ var style = $(
 	"</style>"
 );
 var divContain = $("<div class='tttdivgraph'/>");
-$body.prepend(style);
+/*divContain.draggable();
+divContain.resizable();*/
+//$body.prepend(style);
 $body.prepend(divContain);
 Data.GetAllPlayers(
 	function(players) { 
 		Metier.StoreScores(players, 
 			function(players) {
 				UI.CreerChart(divContain, players);
-				divContain.draggable();
-				divContain.resizable();
 			}
 		);
 	}
