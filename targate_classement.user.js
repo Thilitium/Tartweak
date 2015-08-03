8f10f8da3f172b9a1aa44239525f510b62ec4373
Thilitium <thilitium@gmail.com>
2015-08-03 10:44:26 +0200

    t

--- targate_classement.user.js
+++ targate_classement.user.js
@@ -4,7 +4,7 @@
 // @include 	http://targate.fr/index.php?choix=classement*
 // @include     http://www.targate.fr/index.php?choix=classement*
 // @include     https://targate.fr/index.php?choix=classement*
-// @version     0.0.2.10
+// @version     0.0.2.11
 // @require 	http://code.jquery.com/jquery-2.1.4.min.js
 // @require 	http://git.degree.by/degree/userscripts/raw/bb45d5acd1e5ad68d254a2dbbea796835533c344/src/gm-super-value.user.js
 // @require		https://raw.githubusercontent.com/nnnick/Chart.js/master/Chart.min.js
@@ -97,7 +97,9 @@ var UI = {
 			axisX				: {valueFormatString: "DD/MM/YY"},
 			axisY				: {includeZero: false}, 
 			animationEnabled	: true,
-			data 				: data
+			data 				: data,
+			height				: 300,
+			width				: 500
 		};
 
 		$container.CanvasJSChart(self._chartOptions);
